68ab0e56280dcc9e7c951ac04a7ab78b3a0a4211
Steve Rittgers <rittgerss@wmpenn.edu>
2019-08-29 17:06:45 -0500

    first change - added another print message

--- Testing2/src/SteveProgram.java
+++ Testing2/src/SteveProgram.java
@@ -4,6 +4,7 @@ public class SteveProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing git head vs. master");
+		System.out.println("Made my first change");
 	}
 
 }
